01b0edd4b009b88ea7e844e6ace5183292bf9a64
praveen sankar <praveen sankar>
2017-02-26 19:09:49 +0530

    stack first commit

---
+++ Data-Structure/DS/src/Stack/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(5);
+            Console.WriteLine(stack.Top());
+        }
+    }
+}
---
+++ Data-Structure/DS/src/Stack/Stack.cs
@@ -0,0 +1,50 @@
+﻿namespace Stack
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class Stack<T>
+    {
+        private long top;
+        private long size;
+        private T[] array;
+       public Stack()
+        {
+            size = 1000;
+            array = new T[size];
+            top = -1;
+        }
+
+        public void Push(T data)
+        {
+            if(top>=size)
+            {
+                Console.WriteLine("stack overflow");
+            }
+            array[++top] = data;
+        }
+
+        public T Pop()
+        {
+            if(top<0)
+            {
+                Console.WriteLine("stack underflow");
+                return default(T);
+            }
+            return array[top--];
+
+        }
+
+        public T Top()
+        {
+            if(top<0)
+            {
+                Console.WriteLine("Stack underflow");
+                return default(T);
+            }
+            return array[top];
+        }
+    }
+}
