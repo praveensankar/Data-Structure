b1f1c42b1a2d1671917dc7420f61580e805d4f80
Shankar <praveen.shankar@bt.com>
2017-03-02 10:16:13 +0530

    Tree Traversal

--- Data-Structure/DS/src/BinaryTree/Program.cs
+++ Data-Structure/DS/src/BinaryTree/Program.cs
@@ -9,6 +9,12 @@
             tree.Root = new Node<int>(1);
             tree.Root.Left = new Node<int>(2);
             tree.Root.Right = new Node<int>(3);
+            Console.Write("PreOrder Traversal : ");
+            TreeTraversal<int>.PreOrder(tree.Root);
+            Console.Write("\nInOrder Traversal : ");
+            TreeTraversal<int>.InOrder(tree.Root);
+            Console.Write("\nPostOrder Traversal : ");
+            TreeTraversal<int>.PostOrder(tree.Root);
         }
     }
 }
---
+++ Data-Structure/DS/src/BinaryTree/TreeTraversal.cs
@@ -0,0 +1,36 @@
+﻿namespace BinaryTree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    public static class TreeTraversal<T>
+    {
+        public static void PreOrder(Node<T> root)
+        {
+            if (root == null)
+                return;
+            Console.Write(root.Data + "\t");
+            PreOrder(root.Left);
+            PreOrder(root.Right);
+        }
+
+        public static void InOrder(Node<T> root)
+        {
+            if (root == null)
+                return;
+            PreOrder(root.Left);
+            Console.Write(root.Data + "\t");
+            PreOrder(root.Right);
+        }
+
+        public static void PostOrder(Node<T> root)
+        {
+            if (root == null)
+                return;
+            PostOrder(root.Left);
+            PostOrder(root.Right);
+            Console.Write(root.Data + "\t");
+        }
+    }
+}
